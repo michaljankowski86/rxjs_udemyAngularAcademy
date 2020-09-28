2f4699d91cc643538682d2ec725f9b8effcc94d6
michal.jankowski <michaljankowski86@gmail.com>
2020-09-28 23:44:22 +0200

    initital setup and stream of values

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -1,3 +1,4 @@
+import { interval } from 'rxjs';
 import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 
 @Component({
@@ -10,6 +11,36 @@ export class AboutComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+
+    // document.addEventListener('click', evt => {
+    //   console.log(evt);
+    // });
+
+    // ================================
+
+    // let counter = 0;
+
+    // setInterval(() => {
+
+    //   console.log(counter);
+    //   counter++;
+      
+    // }, 1000);
+
+    // ================================
+
+    // setTimeout(() => {
+
+    //   console.log("finished...");
+      
+    // }, 3000);
+
+    const interval$ = interval(1000);
+
+    interval$.subscribe(val => console.log("stream 1 => " + val));
+
+    interval$.subscribe(val => console.log("stream 2 => " + val));
+    
   }
 
 }
